d84c4e24b9304e7cb6747d47dba9b0cd5851b8c6
Saptak Das <66028584+Saptak625@users.noreply.github.com>
2021-06-30 13:41:39 -0400

    Delete main.sh

--- main.sh
+++
@@ -1,8 +0,0 @@
-pip install -U spacy
-python -m spacy download en_core_web_sm
-pip install flask_wtf
-pip install flask
-pip install markovify
-pip install flask-ckeditor
-pip freeze > requirements.txt
-python main.py
